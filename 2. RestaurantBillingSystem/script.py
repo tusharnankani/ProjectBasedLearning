fa26410d4a0e9d450dd194cf85457514892385ac
tusharnankani <tusharnankani3@gmail.com>
2020-06-29 18:38:47 +0530

    Update Code
    Add Error and Exception handle

--- 2. RestaurantBillingSystem/script.py
+++ 2. RestaurantBillingSystem/script.py
@@ -1,5 +1,7 @@
 from food import Food
 from drink import Drink
+# For error handling;
+import sys
 
 food1 = Food('Sandwich', 50, 330)
 food2 = Food('Chocolate Cake', 40, 450)
@@ -12,26 +14,42 @@ drink2 = Drink('Orange Juice', 20, 350)
 drink3 = Drink('Espresso', 30, 30)
 
 drinks = [drink1, drink2, drink3]
-
+print('--------------------')
 print('Food')
 
-index = 0
-for food in foods:
+for index, food in enumerate(foods):
     print(str(index) + '. ' + food.info())
-    index += 1
 
+print('--------------------')
 print('Drinks')
 
-index = 0
-for drink in drinks:
+for index, drink in enumerate(drinks):
     print(str(index) + '. ' + drink.info())
     index += 1
  
 print('--------------------')
-food_order = int(input('Enter food item number: '))
-selected_food = foods[food_order]
-drink_order = int(input('Enter drink item number: '))
-selected_drink = drinks[drink_order]
+
+while(True):
+    food_order = int(input('Enter food item number: '))
+    
+    try:
+        selected_food = foods[food_order]
+    except:
+        print("Oops!",sys.exc_info()[0],"occured.")
+        print("Please try again!")
+        continue
+
+
+    drink_order = int(input('Enter drink item number: '))
+
+    try:
+        selected_drink = drinks[drink_order]
+    except:
+        print("Oops!",sys.exc_info()[0],"occured.")
+        print("Please try again!")
+        continue
+
+    break
 
 # Take input from the console and assign it to the count variable
 count = int(input('How many meals would you like to purchase? (10% off for 3 or more): '))
@@ -39,6 +57,6 @@ count = int(input('How many meals would you like to purchase? (10% off for 3 or 
 # Call the get_total_price method from selected_food and from selected_drink
 result = selected_food.get_total_price(count) + selected_drink.get_total_price(count)
 
-# Output 'Your total is $____'
+
 print('Your total is Rs. ' + str(result))
 
